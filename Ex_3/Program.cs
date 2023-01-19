bfd078252da4a97b7b5e551c0eb22c3ad6fbc772
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-19 22:09:49 +0300

    Окончание выполнения задачи №3

---
+++ Ex_3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите номер четверти: ");
+int PartNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введенный номер четверти: " + PartNumber);
+
+if (PartNumber == 1)
+{
+    Console.WriteLine("Диапазон возможных координат (+ x; +y)");
+}
+else if (PartNumber == 2)
+{
+    Console.WriteLine("Диапазон возможных координат (- x; +y)");
+}
+else if (PartNumber == 3)
+{
+    Console.WriteLine("Диапазон возможных координат (- x; -y)");
+}
+else if (PartNumber == 4)
+{
+    Console.WriteLine("Диапазон возможных координат (+ x; -y)");
+}
+else
+{
+    Console.WriteLine("Введено некорректное значение четверти!");
+}
