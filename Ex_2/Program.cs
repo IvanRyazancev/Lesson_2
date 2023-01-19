d6ed2088215232cca4c86b19d25f0d29f7062eec
Иван Рязанцев <ryazancev.azov@gmail.com>
2023-01-19 21:42:41 +0300

    Окончание выполнения задачи №2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,23 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+if (numberA == Math.Pow(numberB, 2))
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
+if (numberB == Math.Pow(numberA, 2))
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
